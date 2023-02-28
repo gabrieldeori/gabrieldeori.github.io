7a4e32cbe241d81db5995369c93b1917b5071a0e
gabrieldeori <deori.pessoal@gmail.com>
2023-02-28 00:16:46 -0300

    fix: illustrated respite overflowing

--- src/script.js
+++ src/script.js
@@ -1,12 +1,5 @@
 const C_ACTIVE = 'active';
 
-function adjustVHForAnyBrowser() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-adjustVHForAnyBrowser();
-
 const burguerButton = document.querySelector('.burger_button');
 const dropDownMenu = document.querySelector('.dropdown_menu');
 
